dbe49e32fbcf044fe2e5319bef639cb1b11f4336
huangfrederick <64339052+huangfrederick@users.noreply.github.com>
2025-12-21 19:34:42 +0000

    Deploying to gh-pages from @ huangfrederick/huangfrederick.github.io@143d7f809dc5df47a7a96ed2cc6a5f45e1428368 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -408,15 +408,12 @@ ninja.data = [{
             },},{id: "news-this-portfolio-was-published",
           title: 'This portfolio was published.',
           description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
+          section: "News",},{id: "news-test-announcement-with-details",
+          title: 'test announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "projects-project-1",
+            },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
