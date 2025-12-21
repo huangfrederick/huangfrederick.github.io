45bfbdbd7238df72cbbbd27bf4d5f1b82f1efd33
huangfrederick <64339052+huangfrederick@users.noreply.github.com>
2025-12-21 20:35:54 +0000

    Deploying to gh-pages from @ huangfrederick/huangfrederick.github.io@e33afc4a354e8595bfdaa049856da42fb00c5383 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -4,21 +4,21 @@ const ninja = document.querySelector('ninja-keys');
 // add the home and posts menu items
 ninja.data = [{
     id: "nav-about",
-    title: "about",
+    title: "About",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
     },
   },{id: "nav-projects",
-          title: "projects",
+          title: "Projects",
           description: "A growing collection of your cool projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
           },
         },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          title: "CV",
+          description: "Below is a copy of my CV. You can also view it as PDF with the rightmost button.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
