7eb20e49771757bbae64f47d7895c48893dca7db
huangfrederick <64339052+huangfrederick@users.noreply.github.com>
2025-12-21 17:42:27 +0000

    Deploying to gh-pages from @ huangfrederick/huangfrederick.github.io@354eb23050d58203b9364a4d5153f2d29b8e0021 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-",
-    title: "",
+    id: "nav-about",
+    title: "about",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
@@ -46,7 +46,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
